069a36db1f101a48c621f8ad51089b03f791d53f
Warren <hello@warren.codes>
2022-07-27 16:42:59 -0600

    Add checks for show button and characters.

--- src/components/AnimeListItem.jsx
+++ src/components/AnimeListItem.jsx
@@ -1,17 +1,26 @@
 import useAxios from "../hooks/useAxios";
+import { useState } from 'react';
 
 export default function AnimeListItem (props) {
     const [people] = useAxios(props.people);
 
+    const [showCharacters, setShowCharacters] = useState(false);
+
     return (
         <li>
             <h2>{props.title} ({props.original_title})</h2>
-            <button></button>
-            <ul>
-                {people && people.map(person => <li key={person.id}>
-                    <h3>{person.name}</h3>
-                </li>)}
-            </ul>
+            {people && people.length > 0 &&
+                <>
+                    <button onClick={() => {setShowCharacters(prev => !prev)}}>
+                        Show Characters
+                    </button>
+                    {showCharacters && <ul>
+                        {people.map(person => <li key={person.id}>
+                            <h3>{person.name}</h3>
+                        </li>)}
+                    </ul>}
+                </>
+            }
         </li>
     );
 }
